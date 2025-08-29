a745f5743a7745a5eb6e926686801f51c0966222
Matatcoding <matthew_whitlock@comcast.net>
2025-08-28 21:55:03 -0400

    Filled out app for employees

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import employees from "./db/employees.js";
+
+const app = express();
+
+export default app;
+
+app.route("/").get((req, res) => {
+  res.send("Hello employees!");
+});
+
+app.route("/employees").get((req, res) => {
+  res.send(employees);
+});
+
+app.route("/employees/random").get((req, res) => {
+  const id = Math.floor(Math.random() * employees.length);
+  res.send(employees[id]);
+});
+
+app.route("/employees/:id").get((req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    return res.status(404).send("ID needs to be number");
+  }
+  if (id > employees.length - 1) {
+    return res.status(404).send("Employee not found");
+  }
+
+  res.send(employees[id - 1]);
+});
